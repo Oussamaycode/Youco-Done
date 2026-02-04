fc3fe407f2276471e69944ce1df789adce140150
Oussamaycode <oussamalamlouki047@gmail.com>
2026-02-04 09:28:09 +0100

    feat(db): add 'heure'  column to 'restaurants' table

--- app/database/migrations/2026_02_03_141937_create_reservations_table.php
+++ app/database/migrations/2026_02_03_141937_create_reservations_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('reservation', function (Blueprint $table) {
             $table->id();
+            $table->time('heure_reservation');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('restaurant_id')->constrained();
             $table->timestamps();
         });
     }
