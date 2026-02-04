233f7dea3e3f6abfbfabe7e0b7eb30df1204ec39
Oussamaycode <oussamalamlouki047@gmail.com>
2026-02-04 09:44:52 +0100

    feat(db): add 'url'  column to 'photos' table

--- app/database/migrations/2026_02_03_142256_create_photos_table.php
+++ app/database/migrations/2026_02_03_142256_create_photos_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('photo', function (Blueprint $table) {
             $table->id();
+            $table->string('url');
+            $table->morphs('photoable');
             $table->timestamps();
         });
     }
